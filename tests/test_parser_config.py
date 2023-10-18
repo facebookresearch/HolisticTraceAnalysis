cf7ba0190e3bf01f7b011eb84ea11c80bad87ae9
Xizhou Feng <fengx@meta.com>
2023-10-18 11:18:47 -0400

    fix pre-commit warnings

--- tests/test_parser_config.py
+++ tests/test_parser_config.py
@@ -10,7 +10,9 @@ class ParserConfigTestCase(unittest.TestCase):
         expect_result: List[AttributeSpec]
 
     @classmethod
-    def _compare_config(cls, args1: List[AttributeSpec], args2: List[AttributeSpec]) -> bool:
+    def _compare_config(
+        cls, args1: List[AttributeSpec], args2: List[AttributeSpec]
+    ) -> bool:
         if len(args1) != len(args2):
             return False
         args1.sort(key=lambda a: a.name)
@@ -23,23 +25,34 @@ class ParserConfigTestCase(unittest.TestCase):
     def test_constructor(self):
         t_cases = [
             self._TCase(None, ParserConfig.get_default_args()),
-            self._TCase(ParserConfig.get_minimum_args(), ParserConfig.get_minimum_args())
+            self._TCase(
+                ParserConfig.get_minimum_args(), ParserConfig.get_minimum_args()
+            ),
         ]
 
         for i, tc in enumerate(t_cases):
             cfg = ParserConfig(tc.input)
             got = cfg.get_args()
-            self.assertTrue(self._compare_config(tc.expect_result, got),
-                            f"case #{i}: input={tc.input} expect={tc.expect_result} got={got}")
+            self.assertTrue(
+                self._compare_config(tc.expect_result, got),
+                f"case #{i}: input={tc.input} expect={tc.expect_result} got={got}",
+            )
 
     def test_add_args(self):
         t_cases = [
-            self._TCase(ParserConfig.ARGS_CUDA_MINIMUM, ParserConfig.get_minimum_args()),
-            self._TCase(ParserConfig.ARGS_INPUT_SHAPE, ParserConfig.ARGS_CUDA_MINIMUM + ParserConfig.ARGS_INPUT_SHAPE)
+            self._TCase(
+                ParserConfig.ARGS_CUDA_MINIMUM, ParserConfig.get_minimum_args()
+            ),
+            self._TCase(
+                ParserConfig.ARGS_INPUT_SHAPE,
+                ParserConfig.ARGS_CUDA_MINIMUM + ParserConfig.ARGS_INPUT_SHAPE,
+            ),
         ]
         for i, tc in enumerate(t_cases):
             cfg = ParserConfig(ParserConfig.get_minimum_args())
             cfg.add_args(tc.input)
             got = cfg.get_args()
-            self.assertTrue(self._compare_config(tc.expect_result, got),
-                            f"case #{i}: input={tc.input} expect={tc.expect_result} got={got}")
+            self.assertTrue(
+                self._compare_config(tc.expect_result, got),
+                f"case #{i}: input={tc.input} expect={tc.expect_result} got={got}",
+            )
