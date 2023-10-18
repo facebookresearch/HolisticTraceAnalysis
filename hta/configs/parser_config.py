8860734a2e1f934ffa9a9df91189209886388f8e
Xizhou Feng <fengx@meta.com>
2023-10-18 11:00:28 -0400

    add ParserConfig class and test

---
+++ hta/configs/parser_config.py
@@ -0,0 +1,84 @@
+from enum import Enum
+from typing import TypeVar, Optional, List, NamedTuple
+
+T = TypeVar('T')
+
+
+class ValueType(Enum):
+    """ValueType enumerates the possible data types for the attribute values."""
+    Int = 1
+    Float = 2
+    String = 3
+    Object = 4
+    Timestamp = 5
+    Timedelta = 6
+
+
+class AttributeSpec(NamedTuple):
+    """AttributeSpec specifies what an attribute looks like and how to parse it.
+
+    An AttributeSpec instance has the following fields:
+    + name: the column name used in the output dataframe.
+    + raw_name: the key used in args dict object in the original json trace.
+    + value_type: the expected data type for the values of the attribute.
+    + default_value: what value will be used for missing attribute values.
+    """
+    name: str
+    raw_name: str
+    value_type: ValueType
+    default_value: T
+
+
+class ParserConfig:
+    """TraceParserConfig specifies how to parse a json trace file.
+
+    The current implementation only supports customization on `args` parsing.
+    This class can be extended to support other customizations.
+    """
+    ARGS_INPUT_SHAPE: List[AttributeSpec] = [
+        AttributeSpec("input_dims", "input_dims", ValueType.Object, "-1"),
+        AttributeSpec("input_type", "input_type", ValueType.Object, "-1"),
+    ]
+    ARGS_CUDA_THREADS: List[AttributeSpec] = [
+        AttributeSpec("grid", "grid", ValueType.Object, "-1"),
+        AttributeSpec("block", "block", ValueType.Object, "-1"),
+        AttributeSpec("blocks_per_sm", "blocks_per_sm", ValueType.Int, -1),
+        AttributeSpec("registers_per_thread", "registers_per_thread", ValueType.Int, -1),
+    ]
+    ARGS_BANDWIDTH: List[AttributeSpec] = [
+        AttributeSpec("memory_bw_gbps", "memory bandwidth (GB/s)", ValueType.Float, -1),
+        AttributeSpec("bytes", "bytes", ValueType.Int, -1)
+    ]
+    ARGS_CUDA_MINIMUM: List[AttributeSpec] = [
+        AttributeSpec("stream", "stream", ValueType.Int, -1),
+        AttributeSpec("correlation", "correlation", ValueType.Int, -1)
+    ]
+    ARGS_SYNC:  List[AttributeSpec] = [
+        AttributeSpec("wait_on_stream", "wait_on_stream", ValueType.Int, -1),
+        AttributeSpec("wait_on_cuda_event_record_corr_id", "wait_on_cuda_event_record_corr_id", ValueType.Int, -1),
+    ]
+
+    def __init__(self, args: Optional[List[AttributeSpec]] = None):
+        self.args = []
+        self.set_args(args if args else self.get_default_args())
+
+    @classmethod
+    def get_minimum_args(cls) -> List[AttributeSpec]:
+        return cls.ARGS_CUDA_MINIMUM
+
+    @classmethod
+    def get_default_args(cls) -> List[AttributeSpec]:
+        return cls.ARGS_CUDA_MINIMUM + cls.ARGS_INPUT_SHAPE + cls.ARGS_SYNC + cls.ARGS_BANDWIDTH
+
+    def set_args(self, args: List[AttributeSpec]) -> None:
+        self.args.clear()
+        self.add_args(args)
+
+    def get_args(self) -> List[AttributeSpec]:
+        return self.args
+
+    def add_args(self, args: List[AttributeSpec]) -> None:
+        arg_set = {arg.name for arg in self.args}
+        for arg in args:
+            if arg.name not in arg_set:
+                self.args.append(arg)
---
+++ tests/test_parser_config.py
@@ -0,0 +1,45 @@
+import unittest
+from typing import List, NamedTuple, Optional
+
+from hta.configs.parser_config import AttributeSpec, ParserConfig
+
+
+class ParserConfigTestCase(unittest.TestCase):
+    class _TCase(NamedTuple):
+        input: Optional[List[AttributeSpec]]
+        expect_result: List[AttributeSpec]
+
+    @classmethod
+    def _compare_config(cls, args1: List[AttributeSpec], args2: List[AttributeSpec]) -> bool:
+        if len(args1) != len(args2):
+            return False
+        args1.sort(key=lambda a: a.name)
+        args2.sort(key=lambda a: a.name)
+        for i, arg in enumerate(args1):
+            if arg != args2[i]:
+                return False
+        return True
+
+    def test_constructor(self):
+        t_cases = [
+            self._TCase(None, ParserConfig.get_default_args()),
+            self._TCase(ParserConfig.get_minimum_args(), ParserConfig.get_minimum_args())
+        ]
+
+        for i, tc in enumerate(t_cases):
+            cfg = ParserConfig(tc.input)
+            got = cfg.get_args()
+            self.assertTrue(self._compare_config(tc.expect_result, got),
+                            f"case #{i}: input={tc.input} expect={tc.expect_result} got={got}")
+
+    def test_add_args(self):
+        t_cases = [
+            self._TCase(ParserConfig.ARGS_CUDA_MINIMUM, ParserConfig.get_minimum_args()),
+            self._TCase(ParserConfig.ARGS_INPUT_SHAPE, ParserConfig.ARGS_CUDA_MINIMUM + ParserConfig.ARGS_INPUT_SHAPE)
+        ]
+        for i, tc in enumerate(t_cases):
+            cfg = ParserConfig(ParserConfig.get_minimum_args())
+            cfg.add_args(tc.input)
+            got = cfg.get_args()
+            self.assertTrue(self._compare_config(tc.expect_result, got),
+                            f"case #{i}: input={tc.input} expect={tc.expect_result} got={got}")
