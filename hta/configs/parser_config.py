80e0b2ea9e97abc11440c39618f53730a37ae930
Fei Niu <feiniu@meta.com>
2024-10-10 13:46:16 -0700

    Step2: Add versioning to parserconfig (#188)
    Summary:
    
    - Add versioning to parserconfig. 
    - Hook up yaml parser and the parser config
    
    Differential Revision: D64116676

--- hta/configs/parser_config.py
+++ hta/configs/parser_config.py
@@ -4,7 +4,8 @@ import copy
 from enum import Enum
 from typing import Dict, List, Optional, Set
 
-from hta.configs.default_values import AttributeSpec, ValueType
+from hta.configs.default_values import AttributeSpec
+from hta.configs.event_args_yaml_parser import parse_event_args_yaml, YamlVersion
 
 
 class ParserBackend(str, Enum):
@@ -27,102 +28,11 @@ class TraceType(str, Enum):
     Inference = "inference"
 
 
-AVAILABLE_ARGS: Dict[str, AttributeSpec] = {
-    "index::ev_idx": AttributeSpec("ev_idx", "Ev Idx", ValueType.Int, -1),
-    "index::external_id": AttributeSpec(
-        "external_id", "External id", ValueType.Int, -1
-    ),
-    "cpu_op::concrete_inputs": AttributeSpec(
-        "concrete_inputs", "Concrete Inputs", ValueType.Int, -1
-    ),
-    "cpu_op::fwd_thread": AttributeSpec(
-        "fwd_thread_id", "Fwd thread id", ValueType.Int, -1
-    ),
-    "cpu_op::input_dims": AttributeSpec(
-        "input_dims", "Input Dims", ValueType.Object, "-1"
-    ),
-    "cpu_op::input_type": AttributeSpec(
-        "input_type", "Input type", ValueType.Object, "-1"
-    ),
-    "cpu_op::input_strides": AttributeSpec(
-        "input_strides",
-        "Input Strides",
-        ValueType.Object,
-        "-1",
-    ),
-    "cpu_op::sequence_number": AttributeSpec(
-        "sequence", "Sequence number", ValueType.Int, -1
-    ),
-    # kernel_backend, for example triton.
-    "cpu_op::kernel_backend": AttributeSpec(
-        "kernel_backend", "kernel_backend", ValueType.String, ""
-    ),
-    "correlation::cbid": AttributeSpec("cbid", "cbid", ValueType.Int, -1),
-    "correlation::cpu_gpu": AttributeSpec(
-        "correlation", "correlation", ValueType.Int, -1
-    ),
-    "sm::blocks": AttributeSpec("blocks_per_sm", "blocks per SM", ValueType.Int, -1),
-    "sm::occupancy": AttributeSpec(
-        "est_occupancy", "est. achieved occupancy %", ValueType.Int, -1
-    ),
-    "sm::warps": AttributeSpec("warps_per_sm", "warps per SM", ValueType.Int, -1),
-    "data::bytes": AttributeSpec("bytes", "bytes", ValueType.Int, -1),
-    "data::bandwidth": AttributeSpec(
-        "memory_bw_gbps", "memory bandwidth (GB/s)", ValueType.Int, -1
-    ),
-    "cuda::context": AttributeSpec("context", "context", ValueType.Int, -1),
-    "cuda::device": AttributeSpec("device", "device", ValueType.Int, -1),
-    "cuda::stream": AttributeSpec("stream", "stream", ValueType.Int, -1),
-    "kernel::queued": AttributeSpec("queued", "queued", ValueType.Int, -1),
-    "kernel::shared_memory": AttributeSpec(
-        "shared_memory", "shared memory", ValueType.Int, -1
-    ),
-    "threads::block": AttributeSpec("block", "block", ValueType.Object, "-1"),
-    "threads::grid": AttributeSpec("grid", "grid", ValueType.Int, -1),
-    "threads::registers": AttributeSpec(
-        "registers_per_thread", "registers per thread", ValueType.Int, -1
-    ),
-    "cuda_sync::stream": AttributeSpec(
-        "wait_on_stream", "wait_on_stream", ValueType.Int, -1
-    ),
-    "cuda_sync::event": AttributeSpec(
-        "wait_on_cuda_event_record_corr_id",
-        "wait_on_cuda_event_record_corr_id",
-        ValueType.Int,
-        -1,
-    ),
-    "info::labels": AttributeSpec("labels", "labels", ValueType.String, ""),
-    "info::name": AttributeSpec("name", "name", ValueType.Int, -1),
-    "info::op_count": AttributeSpec("op_count", "Op count", ValueType.Int, -1),
-    "info::sort_index": AttributeSpec("sort_index", "sort_index", ValueType.Int, -1),
-    "nccl::collective_name": AttributeSpec(
-        "collective_name", "Collective name", ValueType.String, ""
-    ),
-    "nccl::in_msg_nelems": AttributeSpec(
-        "in_msg_nelems", "In msg nelems", ValueType.Int, 0
-    ),
-    "nccl::out_msg_nelems": AttributeSpec(
-        "out_msg_nelems", "Out msg nelems", ValueType.Int, 0
-    ),
-    "nccl::group_size": AttributeSpec("group_size", "Group size", ValueType.Int, 0),
-    "nccl::dtype": AttributeSpec("msg_dtype", "dtype", ValueType.String, ""),
-    "nccl::in_split_size": AttributeSpec(
-        "in_split_size", "In split size", ValueType.Object, "[]"
-    ),
-    "nccl::out_split_size": AttributeSpec(
-        "out_split_size", "Out split size", ValueType.Object, "[]"
-    ),
-    "nccl::process_group_name": AttributeSpec(
-        "process_group_name", "Process Group Name", ValueType.String, ""
-    ),
-    "nccl::process_group_desc": AttributeSpec(
-        "process_group_desc", "Process Group Description", ValueType.String, ""
-    ),
-    "nccl::process_group_ranks": AttributeSpec(
-        "process_group_ranks", "Process Group Ranks", ValueType.Object, "[]"
-    ),
-    "nccl::rank": AttributeSpec("process_rank", "Rank", ValueType.Int, -1),
-}
+DEFAULT_PARSE_VERSION: YamlVersion = YamlVersion.v1_0_0
+
+AVAILABLE_ARGS: Dict[str, AttributeSpec] = parse_event_args_yaml(
+    DEFAULT_PARSE_VERSION
+).AVAILABLE_ARGS
 
 
 class ParserConfig:
@@ -142,52 +52,40 @@ class ParserConfig:
     This class can be extended to support other customizations.
     """
 
-    ARGS_INPUT_SHAPE: List[AttributeSpec] = [
-        AVAILABLE_ARGS[k]
-        for k in ["cpu_op::input_dims", "cpu_op::input_type", "cpu_op::input_strides"]
-    ]
-    ARGS_BANDWIDTH: List[AttributeSpec] = [
-        AVAILABLE_ARGS[k] for k in ["data::bytes", "data::bandwidth"]
-    ]
-    ARGS_SYNC: List[AttributeSpec] = [
-        AVAILABLE_ARGS[k] for k in ["cuda_sync::stream", "cuda_sync::event"]
-    ]
-    ARGS_MINIMUM: List[AttributeSpec] = [
-        AVAILABLE_ARGS[k] for k in ["cuda::stream", "correlation::cpu_gpu"]
-    ]
-    ARGS_COMPLETE: List[AttributeSpec] = [
-        AVAILABLE_ARGS[k] for k in AVAILABLE_ARGS if not k.startswith("info")
-    ]
-    ARGS_INFO: List[AttributeSpec] = [
-        AVAILABLE_ARGS[k] for k in ["info::labels", "info::name", "info::sort_index"]
-    ]
-    ARGS_COMMUNICATION: List[AttributeSpec] = [
-        AVAILABLE_ARGS[k]
-        for k in [
-            "nccl::collective_name",
-            "nccl::in_msg_nelems",
-            "nccl::out_msg_nelems",
-            "nccl::dtype",
-            "nccl::group_size",
-            "nccl::rank",
-            "nccl::in_split_size",
-            "nccl::out_split_size",
-        ]
-    ]
-    ARGS_DEFAULT: List[AttributeSpec] = (
-        ARGS_MINIMUM
-        + ARGS_BANDWIDTH
-        + ARGS_SYNC
-        + ARGS_INPUT_SHAPE
-        + [AVAILABLE_ARGS["index::external_id"]]
-    )
+    ARGS_INPUT_SHAPE: List[AttributeSpec] = parse_event_args_yaml(
+        DEFAULT_PARSE_VERSION
+    ).ARGS_INPUT_SHAPE
+    ARGS_BANDWIDTH: List[AttributeSpec] = parse_event_args_yaml(
+        DEFAULT_PARSE_VERSION
+    ).ARGS_BANDWIDTH
+    ARGS_SYNC: List[AttributeSpec] = parse_event_args_yaml(
+        DEFAULT_PARSE_VERSION
+    ).ARGS_SYNC
+    ARGS_MINIMUM: List[AttributeSpec] = parse_event_args_yaml(
+        DEFAULT_PARSE_VERSION
+    ).ARGS_MINIMUM
+    ARGS_COMPLETE: List[AttributeSpec] = parse_event_args_yaml(
+        DEFAULT_PARSE_VERSION
+    ).ARGS_COMPLETE
+    ARGS_INFO: List[AttributeSpec] = parse_event_args_yaml(
+        DEFAULT_PARSE_VERSION
+    ).ARGS_INFO
+    ARGS_COMMUNICATION: List[AttributeSpec] = parse_event_args_yaml(
+        DEFAULT_PARSE_VERSION
+    ).ARGS_COMMUNICATION
+    ARGS_DEFAULT: List[AttributeSpec] = parse_event_args_yaml(
+        DEFAULT_PARSE_VERSION
+    ).ARGS_DEFAULT
 
     def __init__(
         self,
         args: Optional[List[AttributeSpec]] = None,
         user_provide_trace_type: Optional[TraceType] = None,
+        version: YamlVersion = DEFAULT_PARSE_VERSION,
     ) -> None:
-        self.args: List[AttributeSpec] = args if args else self.get_default_args()
+        self.args: List[AttributeSpec] = (
+            args if args else self.get_default_args(version=version)
+        )
         self.parser_backend: Optional[ParserBackend] = None
         self.trace_memory: bool = False
         self.user_provide_trace_type: Optional[TraceType] = user_provide_trace_type
@@ -197,20 +95,30 @@ class ParserConfig:
         return copy.deepcopy(_DEFAULT_PARSER_CONFIG)
 
     @classmethod
+    def get_versioned_cfg(cls, version: YamlVersion) -> "ParserConfig":
+        return copy.deepcopy(ParserConfig(version=version))
+
+    @classmethod
     def set_default_cfg(cls, cfg: "ParserConfig") -> None:
         _DEFAULT_PARSER_CONFIG.set_args(cfg.get_args())
 
     @classmethod
-    def get_minimum_args(cls) -> List[AttributeSpec]:
-        return cls.ARGS_MINIMUM.copy()
+    def get_minimum_args(
+        cls, version: YamlVersion = DEFAULT_PARSE_VERSION
+    ) -> List[AttributeSpec]:
+        return parse_event_args_yaml(YamlVersion.v1_0_0).ARGS_MINIMUM.copy()
 
     @classmethod
-    def get_default_args(cls) -> List[AttributeSpec]:
-        return cls.ARGS_DEFAULT.copy()
+    def get_default_args(
+        cls, version: YamlVersion = DEFAULT_PARSE_VERSION
+    ) -> List[AttributeSpec]:
+        return parse_event_args_yaml(YamlVersion.v1_0_0).ARGS_DEFAULT.copy()
 
     @classmethod
-    def get_info_args(cls) -> List[AttributeSpec]:
-        return cls.ARGS_INFO.copy()
+    def get_info_args(
+        cls, version: YamlVersion = DEFAULT_PARSE_VERSION
+    ) -> List[AttributeSpec]:
+        return parse_event_args_yaml(YamlVersion.v1_0_0).ARGS_INFO.copy()
 
     def set_args(self, args: List[AttributeSpec]) -> None:
         if args != self.args:
@@ -231,8 +139,39 @@ class ParserConfig:
         self.parser_backend = parser_backend
 
     @staticmethod
-    def enable_communication_args() -> None:
-        _DEFAULT_PARSER_CONFIG.add_args(ParserConfig.ARGS_COMMUNICATION)
+    def enable_communication_args(version: YamlVersion = DEFAULT_PARSE_VERSION) -> None:
+        _DEFAULT_PARSER_CONFIG.add_args(
+            parse_event_args_yaml(YamlVersion.v1_0_0).ARGS_COMMUNICATION
+        )
+
+    @staticmethod
+    def set_global_parser_config_version(version: YamlVersion) -> None:
+        global _DEFAULT_PARSER_CONFIG
+        _DEFAULT_PARSER_CONFIG = ParserConfig(version=version)
+
+        global AVAILABLE_ARGS
+        AVAILABLE_ARGS = parse_event_args_yaml(version).AVAILABLE_ARGS
+
+        ParserConfig.ARGS_INPUT_SHAPE = parse_event_args_yaml(
+            DEFAULT_PARSE_VERSION
+        ).ARGS_INPUT_SHAPE
+        ParserConfig.ARGS_BANDWIDTH = parse_event_args_yaml(
+            DEFAULT_PARSE_VERSION
+        ).ARGS_BANDWIDTH
+        ParserConfig.ARGS_SYNC = parse_event_args_yaml(DEFAULT_PARSE_VERSION).ARGS_SYNC
+        ParserConfig.ARGS_MINIMUM = parse_event_args_yaml(
+            DEFAULT_PARSE_VERSION
+        ).ARGS_MINIMUM
+        ParserConfig.ARGS_COMPLETE = parse_event_args_yaml(
+            DEFAULT_PARSE_VERSION
+        ).ARGS_COMPLETE
+        ParserConfig.ARGS_INFO = parse_event_args_yaml(DEFAULT_PARSE_VERSION).ARGS_INFO
+        ParserConfig.ARGS_COMMUNICATION = parse_event_args_yaml(
+            DEFAULT_PARSE_VERSION
+        ).ARGS_COMMUNICATION
+        ParserConfig.ARGS_DEFAULT = parse_event_args_yaml(
+            DEFAULT_PARSE_VERSION
+        ).ARGS_DEFAULT
 
 
 # Define a global ParserConfig variable for internal use. To access this variable,
