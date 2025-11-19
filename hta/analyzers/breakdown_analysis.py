363096023e731270a08fa2c6a9830f3cc6e54f30
Benedykt Bela <benedykt.bela@intel.com>
2025-11-19 11:34:29 +0200

    Add mechaanism to round idle time ration so they sum up to 1.0.
    Signed-off-by: Benedykt Bela <benedykt.bela@intel.com>

--- hta/analyzers/breakdown_analysis.py
+++ hta/analyzers/breakdown_analysis.py
@@ -743,6 +743,21 @@ class BreakdownAnalysis:
             ]
         ]
 
+    def _round_preserving_sum(group: pd.DataFrame) -> pd.DataFrame:
+        """Round idle time ratios while preserving the constraint
+        that they sum to 1.0 (100%) per stream."""
+
+        ratios = group["idle_time_ratio"].round(2)
+        ratio_sum = ratios.sum()
+
+        if ratio_sum != 1.0 and 0 < ratio_sum:
+            max_idx = ratios.idxmax()
+            ratios.loc[max_idx] = ratios.loc[max_idx] + (1.0 - ratio_sum)
+
+        group["idle_time_ratio"] = ratios
+
+        return group
+
     @classmethod
     def _analyze_idle_time_for_stream(
         cls,
@@ -931,6 +946,8 @@ class BreakdownAnalysis:
                 mapper=idle_category_name_map, axis=0, inplace=True
             )
 
+        grouped_result_df = result_df.groupby("stream", group_keys=False)
+        result_df = grouped_result_df.apply(cls._round_preserving_sum)
         result_df = result_df[
             ["rank", "stream", "idle_category", "idle_time", "idle_time_ratio"]
         ].round(2)
